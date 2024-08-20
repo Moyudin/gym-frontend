06d74ac70bed5ef6bdfe36fd0a62df4d6eb1dd22
Moyudin Papu <140317447+Moyudin@users.noreply.github.com>
2024-08-20 06:33:28 +0530

    Update footer.jsx

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -7,13 +7,14 @@ const Footer = () => {
         <div className="footer-content">
           <p>&copy; 2024 FITAPEX. All rights reserved.</p>
           <div className="social-media">
-            <a href="#">
+            <a href="Facebookhttps://m.facebook.comFacebook
+https://www.google.com/url?q=https://m.facebook.com/&sa=U&sqi=2&ved=2ahUKEwixzL2zr4KIAxUhSGwGHSf5HjUQFnoECAkQAQ&usg=AOvVaw3-bCXqX991gVCsRoMEwS-H">
               <i className="fa fa-facebook"></i>
             </a>
-            <a href="#">
+            <a href="https://www.google.com/url?q=https://twitter.com/%3Flang%3Den&sa=U&sqi=2&ved=2ahUKEwjuldHYr4KIAxX2RmwGHbpaHtgQFnoECAkQAQ&usg=AOvVaw10ko7imlFVX8VgCzCyuN56">
               <i className="fa fa-twitter"></i>
             </a>
-            <a href="#">
+            <a href="https://www.google.com/url?q=https://www.instagram.com/&sa=U&sqi=2&ved=2ahUKEwjC5MLqr4KIAxU5RmwGHQIiC4YQFnoECAkQAQ&usg=AOvVaw0r0mDYZkAzitFHNguroVMn">
               <i className="fa fa-instagram"></i>
             </a>
           </div>
